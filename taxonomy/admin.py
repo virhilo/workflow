7c9cfc192fdbab1d72eb09b0ff1f03a868960ce1
kocki <aip@aip.pl>
2012-09-06 23:39:49 +0200

    Many works on start...

--- taxonomy/admin.py
+++ taxonomy/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
-from django.conf import settings
 from django.contrib import admin
+from models import Taxonomy
 
 
 class TaxonomyAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class TaxonomyAdmin(admin.ModelAdmin):
     list_filter = ('dictionary', )
     search_fields = ('term', 'dictionary__term', 'description', )
     save_as = True
+admin.site.register(Taxonomy, TaxonomyAdmin)
