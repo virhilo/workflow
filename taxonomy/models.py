a371e39621997d898f361a4a377d24111afbb76f
kocki <aip@aip.pl>
2012-09-06 22:50:11 +0200

    Many works on start...

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,6 +1,7 @@
 # -*- coding: UTF-8 -*-
-from django.db import models
 import re
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Taxonomy(models.Model):
@@ -9,30 +10,31 @@ class Taxonomy(models.Model):
     application language
     """
     dictionary = models.ForeignKey(
-        'self', related_name='%(class)s_dictionary',
-        verbose_name=u"Dictionary name",
-        help_text=u"Dictionary root element",
+        "self",
+        related_name="%(class)s_dictionary",
+        verbose_name=_("dictionary name"),
+        help_text=_("Dictionary root element"),
     )
     term = models.CharField(
         max_length=100,
         blank=True,
-        verbose_name=u"term",
-        help_text=u"Short definition of term",
+        verbose_name=_("term"),
+        help_text=_("Short definition of term"),
     )
     description = models.CharField(
         max_length=5000,
         blank=True,
-        verbose_name=u"description",
-        help_text=u"Extended description of term, etc.",
+        verbose_name=("description"),
+        help_text=_("Extended description of term, etc."),
     )
     order = models.IntegerField(
         blank=True, null=True,
-        help_text=u"Used for custom ordering",
+        help_text=_("Used for custom ordering"),
     )
     maps = models.ManyToManyField(
         'self',
         blank=True, null=True,
-        related_name='%(class)s_maps',
+        related_name="%(class)s_maps",
         through="TaxonomyMaps",
         symmetrical=False,
     )
@@ -43,36 +45,40 @@ class Taxonomy(models.Model):
     public = models.Manager()
 
     class Meta:
-        verbose_name = u'Dictionary'
-        verbose_name_plural = u'Dictionaries'
+        verbose_name = _("dictionary")
+        verbose_name_plural = _("dictionaries")
         unique_together = [("dictionary", "term", ), ]
-        ordering = ('dictionary__term', 'order',)
+        ordering = ("dictionary__term", "order",)
 
     def __unicode__(self):
         return u'%s' % (self.term)
 
     def description_view(self):
-        '''
+        """
         Clear {key:value} tags from description
-        '''
-        return re.sub(r'\{\w+:[^\}]+\}', '', self.description)
+        """
+        return re.sub(r"\{\w+:[^\}]+\}", "", self.description)
 
     def extra_view(self):
-        '''
+        """
         Extract {key:value} to dict.
-        '''
+        """
         t = {}
-        for mo in re.finditer(r'\{(\w+):([^\}]+)\}', self.description):
+        for mo in re.finditer(r"\{(\w+):([^\}]+)\}", self.description):
             t[mo.group(1)] = mo.group(2)
         return t
 
 
 class TaxonomyFlat(Taxonomy):
+    """
+    String representation of specified dictionary content.
+    """
+
     class Meta:
         proxy = True
 
     def map_view(self):
-        ret = ', '.join(self.maps.values_list('term', flat=True))
+        ret = ", ".join(self.maps.values_list("term", flat=True))
         return ret
 
     def dictionary_view(self):
@@ -81,7 +87,7 @@ class TaxonomyFlat(Taxonomy):
 
 class TaxonomyMaps(models.Model):
     """
-    Used for create subsets related inside taxonomy
+    Used for create subsets related inside taxonomy.
     """
-    label = models.ForeignKey(Taxonomy, related_name='%(class)s_label')
-    map = models.ForeignKey(Taxonomy, related_name='%(class)s_map')
+    label = models.ForeignKey(Taxonomy, related_name="%(class)s_label")
+    map = models.ForeignKey(Taxonomy, related_name="%(class)s_map")
